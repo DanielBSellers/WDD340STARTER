e533bf5ddb1ead193ecd961faa44bbf73406819f
DanielBSellers <sel21002@byui.edu>
2025-01-24 22:42:23 -0700

    learning activity w3

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -22,11 +23,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-
+// Inventory routes
+app.use("/inv", inventoryRoute)
 // Index routes
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
